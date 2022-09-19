0fde522e41993e2f561bdfe26842e8a28569a14b
shilingas <mariussilingas5@gmail.com>
2022-09-19 13:53:34 +0300

    created-fetch-functionality

--- react-dotnet-finance-tracking/ClientApp/src/components/Counter.js
+++ react-dotnet-finance-tracking/ClientApp/src/components/Counter.js
@@ -1,31 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
+export const Counter = () => {
+    const [data, setData] = useState([]);
+    const [status, setStatus] = useState(false);
+    const getData = async () => {
 
-export class Counter extends Component {
-  static displayName = Counter.name;
-
-  constructor(props) {
-    super(props);
-    this.state = { currentCount: 0 };
-    this.incrementCounter = this.incrementCounter.bind(this);
-  }
-
-  incrementCounter() {
-    this.setState({
-      currentCount: this.state.currentCount + 1
-    });
-  }
-
-  render() {
+        const resp = await fetch(`./output.json`);
+        const tempData = await resp.json();
+        setData(tempData);
+        setStatus(true);
+    }
     return (
-      <div>
-        <h1>Counter</h1>
-
-        <p>This is a simple example of a React component.</p>
-
-        <p aria-live="polite">Current count: <strong>{this.state.currentCount}</strong></p>
-
-        <button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
-      </div>
+        <div className='container'>
+            <h1>Getting data: </h1>
+            <button onClick={getData}>test</button>
+            <button onClick={() => setData([])}>clear data</button>
+            <div className='data'>
+                {
+                    status ? (
+                        data.map((item) => {
+                            const { login, id } = item;
+                            return (
+                                <div>
+                                    <h1>{login} </h1>
+                                    <h1>{id}</h1>
+                                </div>
+                            );
+                        })) : (
+                        <h1>empty data...</h1>
+                    )
+                }
+            </div>
+        </div>
     );
-  }
 }
