38903a407be3e7878ad07aeac1b786437926f3b3
Geraldas Minkevičius <geraldasmm@gmail.com>
2022-09-19 21:44:41 +0300

    Semi file reading functionality

---
+++ react-dotnet-finance-tracking/Controllers/BillReader.cs
@@ -0,0 +1,21 @@
+﻿using CsvHelper;
+using System.IO;
+using System.Globalization;
+using System.Linq;
+
+namespace react_dotnet_finance_tracking.Controllers
+{
+    public class BillReader
+    {
+        static void ReadFromFile()
+        {
+            using (var streamReader = new StreamReader(@"C:\Users\geral\Desktop\test.csv"))
+            {
+                using (var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture))
+                {
+                    var records = csvReader.GetRecords<dynamic>().ToList();
+                }
+            }
+        }
+    }
+}
